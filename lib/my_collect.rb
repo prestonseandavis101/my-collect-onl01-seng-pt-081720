c5fa3573d972372a6b8aaac6d63df559507c4110
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-28 01:09:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
-def my_collect(array)
+def my_collect(empty_array)
   i = 0
   name_collection = []
   while i < array.length
-    name_collection.push yield(array[i])
+    name_collection.push yield(empty_array[i])
     i += 1
   end
   name_collection
