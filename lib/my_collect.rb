78d797311822bb25a6709ea0c9ba77fbf36c1a16
Preston Davis <prestonseandavis101@yahoo.com>
2020-08-28 01:04:59 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,5 +1,13 @@
 ["Tim Jones", "Tom Smith", "Jim Campagno"]
 
 def my_collect(empty_array)
+  i = 0 
+  name_collection = []
+  while i < array.length 
+  name_collection << yield (array [i])
+  i += 1
+end
+name_collection
+end
 
 
